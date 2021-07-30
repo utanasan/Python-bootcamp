0612148e318ed130e88cf8e4a167c30daec31a7d
utanasan <80106490+utanasan@users.noreply.github.com>
2021-07-30 14:02:51 +0300

    Add files via upload

---
+++ Reversed string function.py
@@ -0,0 +1,8 @@
+"""Write a function called reverse_string which accepts
+a string and returns a new string with all the characters reversed"""
+
+def reverse_string(desired_string):
+    return desired_string[::-1]
+
+
+print(reverse_string("Mine dying bride"))
