89deb7423c8fb77d2b6d37e871fa4b53a11c2ed2
utanasan <80106490+utanasan@users.noreply.github.com>
2021-08-03 23:45:55 +0300

    Initial

---
+++ simpest_decorator_ever.py
@@ -0,0 +1,13 @@
+
+def print_args(func):
+    def inner_func(*args, **kwargs):
+      print(args)
+      print(kwargs)
+      return func(*args, **kwargs)
+    return inner_func
+
+@print_args
+def multiply(num_a, num_b):
+    return num_a * num_b
+
+print(multiply(3, 5))
